0fdd68e761c4e4cc74b27a49c722ba99e9413dd6
db360 <da.b@hotmail.es>
2022-02-13 13:00:36 +0100

    upload pins

--- social_app_frontend/src/container/Home.jsx
+++ social_app_frontend/src/container/Home.jsx
@@ -10,16 +10,14 @@ import { AiFillCloseCircle } from "react-icons/ai";
 import { Sidebar, UserProfile } from "../components";
 import Pins from "./Pins";
 import { userQuery } from "../utils/data";
+import { fetchUser } from "../utils/fetchUser";
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
